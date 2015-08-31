e28f71e5f70a0561bc97f55da3c58ef31b8d8342
Piotr Czajkowski <czajkowski.piotr@gmail.com>
2015-08-31 20:16:23 +0200

    Project setup. Grunt tasks for HTML, CSS, JS, Server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,76 @@
 /*globals module*/
 
 module.exports = function(grunt) {
-    'use strict';
+    "use strict";
+
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-htmlmin");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-connect");
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
+
+        htmlmin: {
+            dist: {
+              options: {
+                removeComments: true,
+                collapseWhitespace: true
+              },
+              files: {
+                "build/index.html": "src/index.html"
+              }
+            }
+        },
+
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    "build/style.min.css": "src/css/style.css"
+                }
+            }
+        },
+
         uglify: {
             build: {
                 src: [
-                    'src/js/index.js',
-                    'src/js/pacman.js'
+                    "src/js/index.js",
+                    "src/js/pacman.js"
                 ],
-                dest: 'build/<%= pkg.name %>.min.js'
+                dest: "build/<%= pkg.name %>.min.js"
+            }
+        },
+
+        connect: {
+            server: {
+              options: {
+                port: 8000,
+                base: "build",
+                keepalive: true
+              }
             }
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    // Default task(s).
-    grunt.registerTask('default', [
-        'uglify'
+    grunt.registerTask("server", [
+        "connect"
+    ]);
+
+    grunt.registerTask("build", [
+        "htmlmin",
+        "cssmin",
+        "uglify"
+    ]);
+
+
+    grunt.registerTask("default", [
+        "build",
+        "server"
     ]);
 
 };
